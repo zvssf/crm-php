a084c4abc918089faaa73007ae1300a98d0d5c2b
zvssf <danilka.bizzare@gmail.com>
2025-10-24 10:37:32 +0300

    Изменена кнопка в настройках полей страны

--- system/pages/authorized/settings-countries.php
+++ system/pages/authorized/settings-countries.php
@@ -660,7 +660,7 @@ require_once SYSTEM . '/layouts/head.php';
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Готово</button>
                     </div>
                 </div>
             </div>
